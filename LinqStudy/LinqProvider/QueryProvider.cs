881febcfec0591262db193248b09a383a7ad1822
BlueCai <wuyicqb@163.com>
2016-09-20 23:01:37 +0800

    ~part I

---
+++ LinqStudy/LinqProvider/QueryProvider.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqProvider
+{
+    public abstract class QueryProvider : IQueryProvider
+    {
+        public QueryProvider() { }
+
+        IQueryable IQueryProvider.CreateQuery(Expression expression)
+        {
+            Type elementType = TypeSystem.GetElementType(expression.Type);
+
+            try
+            {
+                return (IQueryable)Activator.CreateInstance(typeof(Query<>).MakeGenericType(elementType), new object[] { this, expression });
+            }
+            catch (TargetInvocationException tie)
+            {
+                throw tie.InnerException;
+            }
+        }
+
+        IQueryable<TElement> IQueryProvider.CreateQuery<TElement>(Expression expression)
+        {
+            return new Query<TElement>(this, expression);
+        }
+
+        object IQueryProvider.Execute(Expression expression)
+        {
+            return this.Execute(expression);
+        }
+
+        TResult IQueryProvider.Execute<TResult>(Expression expression)
+        {
+            return (TResult)this.Execute(expression);
+        }
+
+        public abstract string GetQueryText(Expression expression);
+
+        public abstract object Execute(Expression expression);
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # LinqStudy
 
 # This Project is for studing Linq Provider
+
+https://blogs.msdn.microsoft.com/mattwar/2007/07/30/linq-building-an-iqueryable-provider-part-i/
